aed2b72ca5da00e1a2f73f1e8db8de022186cb6a
Juan Manuel Saad <juanmanuel.saad@hotmail.com.ar>
2024-08-24 01:57:32 -0300

    agrego casos bases

--- 1 - Practica Recursion/ejercicio10.py
+++ 1 - Practica Recursion/ejercicio10.py
@@ -1,10 +1,16 @@
 # Escriba una función recursiva que reciba 2 enteros n y b y devuelva True si n es potencia de b
 
 def es_potencia(n: int, b: int) -> bool:
+    # Caso base 1: Si n es 1, es una potencia de b (b^0 = 1).
     if n == 1:
         return True
     
-    if n % b != 0 or n == 0:
+    # Caso base 2: Si n es 0 o b es 0, no es potencia.
+    if n == 0 or b == 0:
+        return False
+    
+    # Caso base 3: Si n no es divisible por b, no puede ser potencia.
+    if n % b != 0:
         return False
     
     return es_potencia(n // b, b)
