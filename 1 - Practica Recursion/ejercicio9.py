007b97d28191473028f9ccc41295e76c75bd746d
Juan Manuel Saad <juanmanuel.saad@hotmail.com.ar>
2024-08-25 02:11:14 -0300

    remuevo variable

--- 1 - Practica Recursion/ejercicio9.py
+++ 1 - Practica Recursion/ejercicio9.py
@@ -1,12 +1,9 @@
 # Escriba una función recursiva que reciba un número positivo n y devuelva la cantidad de dígitos que tiene.
 
 def cantidad_digitos(numero: int) -> int:
-    cantidad = 1
-
     if numero // 10 == 0:
         return 1
     
-    cantidad += cantidad_digitos(numero // 10)
-    return cantidad
+    return 1 + cantidad_digitos(numero // 10)
 
 print(cantidad_digitos(12312))
