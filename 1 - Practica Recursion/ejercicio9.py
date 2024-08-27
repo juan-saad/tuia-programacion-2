c4207645d4aba0fa7e7a141b202e6f4c0581fd44
Juan Manuel Saad <juan-saad@users.noreply.github.com>
2024-08-26 21:31:00 -0300

    Update ejercicio9.py
    optimizo el contador de digitos para utilizar una recursion de tipo cola

--- 1 - Practica Recursion/ejercicio9.py
+++ 1 - Practica Recursion/ejercicio9.py
@@ -6,4 +6,12 @@ def cantidad_digitos(numero: int) -> int:
     
     return 1 + cantidad_digitos(numero // 10)
 
+
+def cantidad_digitos_optimizado(numero: int, b: int = 1) -> int:
+    if numero // 10 == 0:
+        return b
+    
+    return cantidad_digitos_optimizado(numero // 10, b + 1)
+
 print(cantidad_digitos(12312))
+print(cantidad_digitos_optimizado(12312))
