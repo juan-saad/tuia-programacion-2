658c6e87b40b2827f6296d7d0b425d31dc411d95
Juan Manuel Saad <juanmanuel.saad@hotmail.com.ar>
2024-09-22 15:57:25 -0300

    Refactorización de código en función `fibonacci_dicc` para mejorar la legibilidad
    **Descripción:**
    - Eliminados paréntesis innecesarios alrededor de condiciones booleanas en las líneas `if valores.get(n)` y `if not valores.get(n)` para simplificar la sintaxis.
    - No se alteró la lógica del algoritmo ni el comportamiento de las funciones.
    - El código refactorizado mejora la claridad y sigue siendo funcional, manteniendo la misma eficiencia en el cálculo de la secuencia de Fibonacci con memoización.

--- 1 - Practica Recursion/ejercicio18.py
+++ 1 - Practica Recursion/ejercicio18.py
@@ -1,20 +1,21 @@
 def fibonacci(n: int) -> int:
     if n == 0 or n == 1:
         return n
-    
-    return fibonacci(n-1) + fibonacci(n-2)
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
 
 def fibonacci_dicc(n: int, valores: dict = {}) -> int:
     if n == 0 or n == 1:
         valores[n] = n
         return n
-    
-    if (valores.get(n)):
+
+    if valores.get(n):
         return valores[n]
-    
-    if (not valores.get(n)):
-        valores[n] = fibonacci_dicc(n-1, valores) + fibonacci_dicc(n-2, valores)
-    
+
+    if not valores.get(n):
+        valores[n] = fibonacci_dicc(n - 1, valores) + fibonacci_dicc(n - 2, valores)
+
     return valores[n]
 
 
