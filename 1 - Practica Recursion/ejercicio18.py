c1860b9af188c2d30766e957399fcdf17f23385e
Juan Manuel Saad <juanmanuel.saad@hotmail.com.ar>
2024-09-22 16:00:42 -0300

    le agrego la consigna al ejercicio

--- 1 - Practica Recursion/ejercicio18.py
+++ 1 - Practica Recursion/ejercicio18.py
@@ -1,3 +1,10 @@
+# Ya sabemos que la implementación recursiva del cálculo del número de Fibonacci F(0) = 0, F(1) = 1, F(n) = F(n - 1) + F(n - 2)
+# es ineficiente porque muchas de las ramas calculan reiteradamente los mismos valores.
+# Escriba una función fibonacci(n) que calcule el n-ésimo número de Fibonacci de forma recursiva, pero que utilice un diccionario
+# para almacenar los valores ya computados y no computarlos más de una vez.
+# Nota: Será necesario implementar una función wrapper para cumplir con la signatura de la función pedida.
+
+
 def fibonacci(n: int) -> int:
     if n == 0 or n == 1:
         return n
