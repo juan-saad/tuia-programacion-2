b175faeb1aceaa5db8596a8cf6d948f8c139f684
Juan Manuel Saad <juanmanuel.saad@hotmail.com.ar>
2024-11-04 21:31:19 -0300

    Update ejercicio5.py

--- 5 - Arboles/ejercicio5.py
+++ 5 - Arboles/ejercicio5.py
@@ -6,19 +6,38 @@ class Tree:
         
     def preorden(self):
         """Recorrido en preorden: Nodo, Izquierda, Derecha"""
-        print(f'{self.cargo},', end=" ")
+        resultado = [self.cargo]  # Primero, agregar el nodo actual.
+        
         if self.left:
-            self.left.preorden()
+            resultado.extend(self.left.preorden())  # Recursión al subárbol izquierdo
         if self.right:
-            self.right.preorden()
-            
+            resultado.extend(self.right.preorden())  # Recursión al subárbol derecho
+        
+        return resultado
+
     def inorden(self):
         """Recorrido en inorden: Izquierda, Nodo, Derecha"""
+        resultado = []
+        
         if self.left:
-            self.left.inorden()
-        print(self.cargo, end=" ")
+            resultado.extend(self.left.inorden())  # Recursión al subárbol izquierdo
+        resultado.append(self.cargo)  # Luego, agregar el nodo actual.
         if self.right:
-            self.right.inorden()
+            resultado.extend(self.right.inorden())  # Recursión al subárbol derecho
+        
+        return resultado
+
+    def postorden(self):
+        """Recorrido en postorden: Izquierda, Derecha, Nodo"""
+        resultado = []
+        
+        if self.left:
+            resultado.extend(self.left.postorden())  # Recursión al subárbol izquierdo
+        if self.right:
+            resultado.extend(self.right.postorden())  # Recursión al subárbol derecho
+        resultado.append(self.cargo)  # Finalmente, agregar el nodo actual.
+        
+        return resultado
         
     def __str__(self) -> str:
         return str(self.cargo)
