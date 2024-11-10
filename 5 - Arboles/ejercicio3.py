e5c84c4a896c581a14f583517a818d2ec0d70182
Juan Manuel Saad <juanmanuel.saad@hotmail.com.ar>
2024-11-10 16:56:28 -0300

    ejercicio 3

---
+++ 5 - Arboles/ejercicio3.py
@@ -0,0 +1,104 @@
+from typing import Any
+
+
+class Tree:
+    def __init__(self, cargo: Any, left: "Tree" = None, right: "Tree" = None):
+        self.cargo = cargo
+        self.left = left
+        self.right = right
+
+    def nodes(self) -> int:
+        count = 1
+        if self.left:
+            count += self.left.nodes()
+        if self.right:
+            count += self.right.nodes()
+        return count
+
+    def min_max(self) -> tuple[Any, Any]:
+        min_val = max_val = self.cargo
+
+        if self.left:
+            left_min, left_max = self.left.min_max()
+            min_val = min(min_val, left_min)
+            max_val = max(max_val, left_max)
+
+        if self.right:
+            right_min, right_max = self.right.min_max()
+            min_val = min(min_val, right_min)
+            max_val = max(max_val, right_max)
+
+        return (min_val, max_val)
+
+    def search(self, elem: Any) -> bool:
+        if self.cargo == elem:
+            return True
+
+        if self.left and self.left.search(elem):
+            return True
+
+        if self.right and self.right.search(elem):
+            return True
+
+        return False
+
+    def height(self) -> int:
+        if not self:
+            return -1
+
+        left_height = self.left.height() if self.left else -1
+        right_height = self.right.height() if self.right else -1
+
+        return 1 + max(left_height, right_height)
+
+    def __str__(self) -> str:
+        return str(self.cargo)
+
+
+def preorden(tree: "Tree"):
+    """Recorrido en preorden: Nodo, Izquierda, Derecha"""
+    resultado = [tree.cargo]
+
+    if tree.left:
+        resultado.extend(preorden(tree.left))
+    if tree.right:
+        resultado.extend(preorden(tree.right))
+
+    return resultado
+
+
+def inorden(tree: "Tree"):
+    """Recorrido en inorden: Izquierda, Nodo, Derecha"""
+    resultado = []
+
+    if tree.left:
+        resultado.extend(inorden(tree.left))
+    resultado.append(tree.cargo)
+    if tree.right:
+        resultado.extend(inorden(tree.right))
+
+    return resultado
+
+
+def postorden(tree: "Tree"):
+    """Recorrido en postorden: Izquierda, Derecha, Nodo"""
+    resultado = []
+
+    if tree.left:
+        resultado.extend(postorden(tree.left))
+    if tree.right:
+        resultado.extend(postorden(tree.right))
+    resultado.append(tree.cargo)
+
+    return resultado
+
+
+tree = Tree(
+    10,
+    Tree(5, Tree(2), Tree(7, Tree(30, None, Tree(1, None, Tree(100))))),
+    Tree(15, Tree(12, None, Tree(20)), Tree(18)),
+)
+
+print(preorden(tree))
+print(postorden(tree))
+print(inorden(tree))
