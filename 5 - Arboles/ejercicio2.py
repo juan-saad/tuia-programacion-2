967ff9b1a42f4ece7289a27e34bd3a06aedfc496
Juan Manuel Saad <juanmanuel.saad@hotmail.com.ar>
2024-11-10 15:25:40 -0300

    ejercicio 2

---
+++ 5 - Arboles/ejercicio2.py
@@ -0,0 +1,45 @@
+from typing import Any
+
+
+class Tree:
+    def __init__(self, cargo: Any, left: "Tree" = None, right: "Tree" = None):
+        self.cargo = cargo
+        self.left = left
+        self.right = right
+
+    def nodes(self) -> int:
+        count = 1
+        if self.left:
+            count += self.left.nodes()
+        if self.right:
+            count += self.right.nodes()
+        return count
+
+    def min_max(self) -> tuple[Any, Any]:
+        min_val = max_val = self.cargo
+
+        if self.left:
+            left_min, left_max = self.left.min_max()
+            min_val = min(min_val, left_min)
+            max_val = max(max_val, left_max)
+
+        if self.right:
+            right_min, right_max = self.right.min_max()
+            min_val = min(min_val, right_min)
+            max_val = max(max_val, right_max)
+
+        return (min_val, max_val)
+
+    def search(self, elem: Any) -> bool:
+        pass
+
+    def height(self) -> int:
+        pass
+
+    def __str__(self) -> str:
+        return str(self.cargo)
+
+
+tree = Tree(10, Tree(5, Tree(2), Tree(7, Tree(30, None, Tree(1, None, Tree(100))))), Tree(15, Tree(12, None, Tree(20)), Tree(18)))
+
+print(tree.min_max())
